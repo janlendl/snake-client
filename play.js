f9f4524d44ff3f3c24622d1dc28d873df1a26abe
Lendl Peralta <janlendl@gmail.com>
2021-08-18 22:22:23 +0000

    added event handler ctrl+c to terminate the program

--- play.js
+++ play.js
@@ -2,3 +2,24 @@
 const { connect } = require('./client');
 console.log('Connecting ... ');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') process.exit(); // this will make sure we terminate the terminal with CTRL+C
+}
+
