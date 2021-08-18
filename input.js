4c45cf228e7ef7b855815f6fb84d55eb5b88937f
Lendl Peralta <janlendl@gmail.com>
2021-08-18 22:46:39 +0000

    created new js file to move stdin functions we created

---
+++ input.js
@@ -0,0 +1,21 @@
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user via stdin
+ */
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') process.exit(); // this will make sure we terminate the terminal with CTRL+C
+}
+
+module.exports = { setupInput };
