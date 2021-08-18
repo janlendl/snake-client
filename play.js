e50813367a8135c4e719e958b55510642f0b266c
Lendl Peralta <janlendl@gmail.com>
2021-08-18 18:11:05 +0000

    removed connect function and moved it to client.js file

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require('net');
 
-// Establish connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // Interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ... ');
 connect();
