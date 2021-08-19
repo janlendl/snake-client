8b9dac4a622db0c4927bf57634a5d92697876f22
Lendl Peralta <janlendl@gmail.com>
2021-08-19 01:28:25 +0000

    binded w,a,s,d to console up, left, down, right using switch statement

--- input.js
+++ input.js
@@ -1,8 +1,12 @@
+// Stores the active TCP connection object
+let connection;
+
 /**
  * Setup User Interface
  * Specifically, so that we can handle user via stdin
  */
- const setupInput = function() {
+ const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -15,7 +19,24 @@
 }
 
 const handleUserInput = (key) => {
-  if (key === '\u0003') process.exit(); // this will make sure we terminate the terminal with CTRL+C
+  // this will make sure we terminate the terminal with CTRL+C
+  switch (key) {
+    case '\u0003':
+      process.exit();
+      break;
+    case 'w':
+      console.log('up');
+      break;
+    case 'a':
+      console.log('left');
+      break;
+    case 's':
+      console.log('down');
+      break;
+    case 'd':
+      console.log('right');
+      break;
+  }
 }
 
 module.exports = { setupInput };
